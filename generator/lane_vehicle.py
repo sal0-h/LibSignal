abb23a01b8567c2739ffe5785a44db01207a8549
Madina <mmirzata@andrew.cmu.edu>
2026-06-21 14:29:19 +0300

    another bug fix

--- generator/lane_vehicle.py
+++ generator/lane_vehicle.py
@@ -103,7 +103,7 @@ class LaneVehicleGenerator(BaseGenerator):
                     tmp = sorted(I.road_lane_mapping[r], key=lambda ob: int(ob[-1]))
                 self.lanes.append(tmp)
                 # TODO: rank lanes by lane ranking [0,1,2], assume we only have one digit for ranking
-        elif isinstance(world, world_cityflow.World):
+        elif world_cityflow is not None and isinstance(world, world_cityflow.World):
             for road in roads:
                 from_zero = (road["startIntersection"] == I.id) if self.world.RIGHT else (road["endIntersection"] == I.id)
                 self.lanes.append([road["id"] + "_" + str(i) for i in range(len(road["lanes"]))[::(1 if from_zero else -1)]])
