5b083c22ef38ec5eee82c244bc9c80f0bb479718
Madina <mmirzata@andrew.cmu.edu>
2026-07-06 12:57:47 +0300

    IPPO: bump SLURM time to 24h, reduce episodes to 500 for 4x4 hetero

--- extras/slurm_ppo_hetero.sh
+++ extras/slurm_ppo_hetero.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ippo_hetero_4x4
 #SBATCH --output=logs/ippo_hetero_4x4_%j.out
 #SBATCH --error=logs/ippo_hetero_4x4_%j.err
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
 #SBATCH --nodes=1
